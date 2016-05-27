a55475c7b1989891e0f9f727c8d0d1f7c416d7f5
Johannes Deml <send@johannesdeml.com>
2016-05-27 19:46:18 +0200

    Add raw data information as text in ui

---
+++ Assets/Scripts/AccelerometerText.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine.UI;
+
+namespace Supyrb
+{
+	using UnityEngine;
+	using System.Collections;
+
+    [RequireComponent(typeof(Text))]
+	public class AccelerometerText : MonoBehaviour
+    {
+        private Text text;
+		void OnEnable ()
+		{
+		    text = GetComponent<Text>();
+		}
+		
+		void FixedUpdate ()
+		{
+		    Vector3 acc = Input.acceleration;
+		    text.text = String.Format("(X:{0} Y:{1} Z:{2})", 
+                acc.x.ToString("0.00"), acc.y.ToString("0.00"), acc.z.ToString("0.00"));
+		}
+	}
+}
---
+++ Assets/Scripts/GyroText.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine.UI;
+
+namespace Supyrb
+{
+	using UnityEngine;
+	using System.Collections;
+
+	public class GyroText : MonoBehaviour 
+	{
+        private Text text;
+        void OnEnable()
+        {
+            text = GetComponent<Text>();
+        }
+
+        void FixedUpdate()
+        {
+            Quaternion q = Input.gyro.attitude;
+            q.z *= -1f;
+            Vector3 gyro = q.eulerAngles;
+            text.text = String.Format("(X:{0} Y:{1} -Z:{2})",
+                gyro.x.ToString("0"), gyro.y.ToString("0"), gyro.z.ToString("0"));
+        }
+    }
+}
